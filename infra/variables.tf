625e6da2e350ac5b231b8dc4c12cfb1afec77af9
David Jacobs <davidjacobs522@gmail.com>
2024-08-03 02:47:38 -0400

    modified config

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,8 +7,3 @@ variable "table_name" {
   type    = string
   default = "resume-table"
 }
-
-variable "remote_state_bucket" {
-  type = string
-  description = "s3 bucket for the remote state file"
-}
